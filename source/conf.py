c26a4d5b2a959e26faa2cc76f05188f2445197c5
maitaleonida <157442940+maitaleonida@users.noreply.github.com>
2026-01-05 19:06:41 +0800

    Update conf.py with the latest version

--- source/conf.py
+++ source/conf.py
@@ -2,9 +2,9 @@
 
 
 project = 'Inoculator User Guide'
-copyright = '2025, SignatureIP'
+copyright = '2026, SignatureIP'
 author = 'SignatureIP'
-release = '5.4.3'
+release = '5.4.4'
 
 # -- General configuration ---------------------------------------------------
 
@@ -35,7 +35,7 @@ html_theme_options = {
     "use_fullscreen_button": True,
     "use_sidenotes": True,
     "show_toc_level": 3,
-    "announcement": "Inoculator User Guide 5.4.3",
+    "announcement": "Inoculator User Guide 5.4.4",
     "home_page_in_toc": True,
     "repository_url": "https://github.com/signatureip/inoculator-guide",
     "repository_branch": "main",
